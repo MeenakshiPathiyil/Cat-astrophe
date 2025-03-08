183a11c5c8506e497f748da88496bd437f28bf22
yashmithaa <syash.mitha94@gmail.com>
2025-03-09 05:06:35 +0530

    added popup.html and popup.js

---
+++ popup.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const imageElement = document.getElementById("catImage");
+    const messageElement = document.getElementById("message");
+
+    
+    const messages = ["Judging you...", "Good job!"];
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    messageElement.textContent = randomMessage;
+
+    
+    // try {
+    //     const response = await fetch("https://source.unsplash.com/?cat");
+    //     imageElement.src = response.url;
+    // } catch (error) {
+    //     console.error("Failed to fetch cat image:", error);
+    //     messageElement.textContent = "Error fetching sass!";
+    // }
+});
